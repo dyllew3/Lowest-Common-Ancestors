acb41126d9ed5a4bd7b078731bae3be250d79897
dyllew3 <digitald83@gmail.com>
2017-10-26 18:08:10 +0100

    Add test which checks that graph's size attribute increases when a uniques node is added to the graph, and DFS nested class in DAG class which performs depth first search and checks for cycles, altered connect to not add edge if a cycle is detected. Appropriate tests were added for each addition

--- DAG.java
+++ DAG.java
@@ -5,10 +5,46 @@ import java.util.HashSet;
 public class DAG<E extends Comparable<E>> implements Graph<E> {
 
     
-    public HashMap<E,HashSet<E>> vertices;
+    private HashMap<E,HashSet<E>> vertices;
     private int size;
 
+    private class DFS{
+	
+	private HashSet<E> tempMark;
+	private HashSet<E> permMark;
+	public boolean cycle;
+	private ArrayList<E> L;
+	
+	public DFS(HashSet<E> nodes){
+	    tempMark = new HashSet<E>();
+	    permMark = new HashSet<E>();
+	    L = new ArrayList<E>();
+	    this.cycle = false;
+	    for(E node : nodes){
+		dfs(node);
+	    }
+	}
 
+	public void dfs(E node){
+	    if(permMark.contains(node)){
+		return;
+	    }
+	    if(tempMark.contains(node)){
+		this.cycle = true;
+		return;
+	    }
+	    tempMark.add(node);
+	    for(E child : getEdges(node)){
+		dfs(child);
+	    }
+	    permMark.add(node);
+	    L.add(node);
+	}
+	
+	
+    } 
+
+    
     public DAG(){
 	this.size = 0;
 	this.vertices = new HashMap<E, HashSet<E>>();
@@ -24,9 +60,35 @@ public class DAG<E extends Comparable<E>> implements Graph<E> {
     public void insert(E val){
 	if(this.vertices.get(val) == null){
 	    this.vertices.put(val, new HashSet<E>());
+	    this.size++;
 	}
     }
 
+    public HashSet<E> getEdges(E val){
+	return this.vertices.get(val);
+    }
     
 
+    public void connect(E v, E w){
+	this.insert(v);
+	this.insert(w);
+	HashSet<E> vw = this.vertices.get(v);
+	if(!vw.contains(w)){
+	    vw.add(w);
+	}
+	if(hasCycle()){
+	    vw.remove(w);
+	}
+	this.vertices.put(v, vw);
+    }
+
+    public boolean hasCycle(){
+	DFS cycle = new DFS( new HashSet<E>(this.vertices.keySet()));
+	return cycle.cycle;
+    }
+
+    public int getSize(){
+	return this.size;
+    }
 }
+
--- TestDAG.java
+++ TestDAG.java
@@ -40,7 +40,7 @@ public class TestDAG {
 	    DAG<Integer> dag = new DAG<Integer>();
 	    for(int val : tests){
 		dag.insert(val);
-		assertNotNull(dag.vertices.get(val));
+		assertNotNull(dag.getEdges(val));
 	    }
 	}
     }
@@ -62,10 +62,74 @@ public class TestDAG {
 	for (int[] tests :testVals){	
 	    DAG<Integer> dag = new DAG<Integer>();
 	    for(int  i = 0; i < tests.length - 1 ; i++){
-		assertNull(dag.vertices.get(tests[i + 1]));
+		assertNull(dag.getEdges(tests[i + 1]));
 		dag.insert(tests[i]);
-		assertNotNull(dag.vertices.get(tests[i]));
+		assertNotNull(dag.getEdges(tests[i]));
 	    }
+	}
+
+    }
+
+    @Test
+    public void testGetSize(){
+	int[][] testVals = {
+	    {},
+	    {0},
+	    {1,2},
+	    {1, 2, 3, 4},
+	    {10, 9, 7, 6, 5, 4, 3, 2, 1},
+	    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+	    {1, 4, 25, 3, 5, 64, 6, 7},
+	    //Duplicates
+	    {1,1,1,2,2,3,5,3,}
+	};
+	int index = 0;
+	int [] sizes = {0,1,2,4,9,10,8,4};
+	for (int[] tests :testVals){	
+	    DAG<Integer> dag = new DAG<Integer>();
+	    for(int  i = 0; i < tests.length; i++){
+		dag.insert(tests[i]);
+	    }
+	    assertEquals(sizes[index++], dag.getSize());
+	}
+	
+    }
+    
+    @Test
+    public void testConnectNoCycles(){
+	int[][] testVals = {
+	    {10, 9, 7, 6, 5, 4, 3, 2, 1},
+	    {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+	    {1, 4, 25, 3, 5, 64, 6, 7},
+	};
+	for (int[] tests :testVals){	
+	    for(int  i = 0; i < tests.length - 1 ; i++){
+		DAG<Integer> dag = new DAG<Integer>();
+		dag.insert(tests[i + 1]);
+		assertFalse(dag.getEdges(tests[i + 1]).contains(tests[i]));
+		dag.connect(tests[i + 1], tests[i]);
+		assertTrue(dag.getEdges(tests[i + 1]).contains(tests[i]));
+	    }
+	}
+    }
+
+    @Test
+    public void testConnectCycles(){
+	int[][] testVals = {
+	    {1,2,3,1},
+	    {1,2,2,4,3,4,1},
+	    {1,2,3,4,5,6,2,9},
+	    // Multiple cycles
+	    {1,2,4,1,3,5,6,7,5,3}
+	};
+	
+	for (int[] tests :testVals){
+	    DAG<Integer> dag = new DAG<Integer>();
+	    for(int  i = 0; i < tests.length - 1 ; i++){
+		dag.connect(tests[i + 1], tests[i]);
+	    }
+	    assertFalse(dag.hasCycle());
+
 	}
 
     }
